8faf46d98ca4205dabf07007015d622cdd83c8c4
littlewhywhat <littlewhywhat@gmail.com>
2016-09-06 17:00:23 +0200

    refactored, added class Graph

--- src/program.cpp
+++ src/program.cpp
@@ -1,48 +1,68 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <cstdint>
 
 class Node;
-class DEdge;
 
-class DEdge {
+class Edge {
   public:
-    DEdge(Node * to, char eVal) : m_Val(eVal), m_To(to) {}
-    char m_Val;
-    Node * m_To;
+    Edge(Node * node, char label) : m_Label(label), m_Node(node) {}
+    char m_Label;
+    Node * m_Node;
 };
 
 class Node {
+  private:
+    uint32_t m_Val;
+    std::vector<Edge*> m_Out;
   public:
-    char m_Val;
-    std::vector<DEdge*> edges;
-    Node(char val) : m_Val(val) {}
+    Node(uint32_t val) : m_Val(val) {}
     ~Node() {
-        for (DEdge * edge : edges)
+        for (Edge * edge : m_Out)
             delete edge;
     }
-    Node * addEdge(Node * node, char eVal) {
-        edges.push_back(new DEdge(node, eVal));
-        return this;
+    uint32_t val() {
+        return m_Val;
+    }
+    void add_out(Node * node, char label) {
+        m_Out.push_back(new Edge(node, label));
+    }
+    const std::vector<Edge*> & out() const {
+        return m_Out;
     }
 };
-   
-void read(std::vector<Node *> & graph) {
-    Node * a = new Node('a');
-    Node * b = new Node('b');
-    Node * c = new Node('c');
-    Node * d = new Node('d');
-    a -> addEdge(c, '0');
-    a -> addEdge(b, '1');
-    b -> addEdge(d, '0');
-    c -> addEdge(d, '0');
-    c -> addEdge(b, '1');
-    d -> addEdge(a, '0');
-    
-    graph.push_back(a);
-    graph.push_back(b);
-    graph.push_back(c);
-    graph.push_back(d);
+
+class Graph {
+  private:
+    std::vector<Node*> m_Nodes;
+  public:
+    Graph(uint32_t size) {
+        std::cout << "graph" << std::endl;
+        for (uint32_t i = 0; i < size; i++)
+            m_Nodes.push_back(new Node(i));
+    }
+    ~Graph() {
+        for (Node * node : m_Nodes)
+            delete node;
+    }
+    void addEdge(uint32_t from, uint32_t to, char label) {
+        m_Nodes.at(from) -> add_out(m_Nodes.at(to), label);
+    }
+    const std::vector<Node*> & nodes() const {
+        return m_Nodes;
+    }
+};
+ 
+Graph read() {
+    Graph graph(4);
+    graph.addEdge(0, 1, '0');
+    graph.addEdge(0, 1, '1');
+    graph.addEdge(1, 2, '0');
+    graph.addEdge(1, 3, '1');
+    graph.addEdge(2, 3, '1');
+    graph.addEdge(3, 0, '0');
+    return graph;
 }
 
 void recurse(Node * node, int & count, int & lim, std::string & str) {
@@ -50,10 +70,10 @@ void recurse(Node * node, int & count, int & lim, std::string & str) {
         std::cout << str <<  std::endl;
         return;
     }
-    for (DEdge * edge : node -> edges) {
+    for (Edge * edge : node -> out()) {
         count += 1;
-        str.push_back(edge -> m_Val);
-        recurse(edge -> m_To, count, lim, str);
+        str.push_back(edge -> m_Label);
+        recurse(edge -> m_Node, count, lim, str);
         count -= 1;
         str.pop_back();
     }
@@ -62,16 +82,12 @@ void recurse(Node * node, int & count, int & lim, std::string & str) {
 int main (void) {
     int lim = 3;
     std::cout << "calculating... " << std::endl;
-    std::vector<Node *> graph;
-    read(graph);
+    Graph graph = read();
     int count = 0;
-    std::string str;
-    for (Node * node : graph) {
-       std::cout << node -> m_Val << ":" << std::endl;
-       recurse(node, count, lim, str); 
+    std::string buffer;
+    for (Node * node : graph.nodes()) {
+       std::cout << node -> val() << ":" << std::endl;
+       recurse(node, count, lim, buffer); 
     }
-    for (Node * node : graph) 
-       delete node; 
     return 0;
-
 }
