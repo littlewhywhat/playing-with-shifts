2a48bfea261eaa41da985bbfc3c37a9ea10fab85
littlewhywhat <littlewhywhat@gmail.com>
2016-10-04 23:15:51 +0200

    renamed mode3::recurse_gs to good_strat_rem

--- src/mode3.h
+++ src/mode3.h
@@ -8,7 +8,7 @@
 class Mode3 : public Mode {
   private:
     const WordTree * m_WT;
-    bool recurse_gs(const TNode & node, uint64_t rem_strat, uint32_t wordlen) const {
+    bool good_strat_rem(const TNode & node, uint64_t rem_strat, uint32_t wordlen) const {
         if (node.is_leaf()) {
             if (wordlen == 0)
                 return true;
@@ -17,11 +17,11 @@ class Mode3 : public Mode {
         }
         if (rem_strat & 1)
             return node.has_zero() && node.has_one() &&
-                   recurse_gs(node.zero(), rem_strat >> 1, wordlen - 1) && 
-                   recurse_gs(node.one(), rem_strat >> 1, wordlen - 1);
+                   good_strat_rem(node.zero(), rem_strat >> 1, wordlen - 1) && 
+                   good_strat_rem(node.one(), rem_strat >> 1, wordlen - 1);
         else
-            return (node.has_zero() && recurse_gs(node.zero(), rem_strat >> 1, wordlen - 1)) || 
-                   (node.has_one() && recurse_gs(node.one(), rem_strat >> 1, wordlen - 1));
+            return (node.has_zero() && good_strat_rem(node.zero(), rem_strat >> 1, wordlen - 1)) || 
+                   (node.has_one() && good_strat_rem(node.one(), rem_strat >> 1, wordlen - 1));
     }
     const WordTree & get_wt() const {
         return *m_WT;
@@ -31,7 +31,7 @@ class Mode3 : public Mode {
         return m_WT;
     }
     bool good_strat_ch(const Strategy & s) const override {
-        return recurse_gs(get_wt().root(), s.val(), s.length());    
+        return good_strat_rem(get_wt().root(), s.val(), s.length());    
     }
   public:
     void set_wd(const WordTree & wt) override {
