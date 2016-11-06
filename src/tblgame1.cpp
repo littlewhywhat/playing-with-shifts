2cccb7779d6cedb65cfa0be2aae60f739ee3af3a
littlewhywhat <littlewhywhat@gmail.com>
2016-11-06 18:25:02 +0100

    changed auto to const auto &

--- src/tblgame1.cpp
+++ src/tblgame1.cpp
@@ -9,7 +9,7 @@ bool TblGame1::Less::operator() (const uint64_t & a, const uint64_t & b) const {
 bool TblGame1::play_tm(const uint64_t & max_comb_val, const uint64_t & s_val) const {
     Less less(s_val);
     std::set<uint64_t, Less> combset(less);
-    for (auto word : tbllang()) 
+    for (const auto & word : tbllang()) 
         if (combset.find(word) == combset.end()) {
             combset.insert(word);
             if (combset.size() == max_comb_val)
--- src/tblgame2.cpp
+++ src/tblgame2.cpp
@@ -12,7 +12,7 @@ bool TblGame2::InverseLess::operator() (const uint64_t & a, const uint64_t & b) 
 bool TblGame2::play_tm(const uint64_t & max_comb_val, const uint64_t & s_val) const {
     InverseLess c(s_val);
     std::map<uint64_t, uint64_t, InverseLess> words_map(c);
-    for (auto word : tbllang()) {
+    for (const auto & word : tbllang()) {
         auto search = words_map.find(word);
         if (search == words_map.end()) 
             words_map.insert(std::make_pair(word, 1)); 
