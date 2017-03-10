19cfe9faf1a7d613d86e0db30636fd55c32d209d
Roman Vaivod <vaivorom@fit.cvut.cz>
2017-03-10 18:27:26 +0100

    implemented isomorphism check

--- universe_script.cpp
+++ universe_script.cpp
@@ -7,6 +7,7 @@
 #include <cmath>
 #include <string>
 #include <algorithm>
+#include <map>
 
 class GraphMatrix {
   private:
@@ -35,6 +36,28 @@ class GraphMatrix {
 	    return visited.size();
 	}
 
+	bool isomorphic(GraphMatrix & identity, int i, std::set<std::string> & graph_strs) {
+		if (i == m_Size) {
+			std::string isomorph = identity.multiply(*this).multiply(identity).print().to_string();
+			return graph_strs.find(isomorph) != graph_strs.end();
+		}
+		for (int j = 0; j < m_Size; j++) {
+			bool is_occupied = false;
+			for (int k = 0; k < i; k++)
+				if (identity.v()[k][j]) {
+					is_occupied = true;
+					break;
+				}
+			if (!is_occupied) {
+				identity.v()[i][j] = 1;
+				if (isomorphic(identity, i + 1, graph_strs))
+					return true;
+				identity.v()[i][j] = 0;
+			}
+		}
+		return false;
+	}
+
   public:
 	GraphMatrix(int size) : m_Size(size) {}
 	std::string to_string() {
@@ -44,12 +67,14 @@ class GraphMatrix {
 				s.push_back(m_Values[i][j] + 48);
 		return s;
 	}
-	void print() {
+	GraphMatrix & print() {
 		for (int i = 0; i < m_Size; i++) {
 			for (int j = 0; j < m_Size; j++)
 				std::cout << m_Values[i][j] << ' ';
 			std::cout << std::endl;
 		}
+		std::cout << std::endl;
+		return *this;
 	}
     matrix & v() {
     	return m_Values;
@@ -59,6 +84,9 @@ class GraphMatrix {
     }
     void push_row(std::vector<int> row) {
     	m_Values.push_back(row);
+    }
+    void pop_row() {
+    	m_Values.pop_back();
     }
 
     bool isSCGraph() {
@@ -81,9 +109,45 @@ class GraphMatrix {
             }
         }
     }
+
+    bool isomorphic_to(std::set<std::string> & graph_strs)  {
+    	GraphMatrix identity(m_Size);
+    	for (int i = 0; i < m_Size; i++) {
+    		std::vector<int> row;
+    		for (int j = 0; j < m_Size; j++)
+    			row.push_back(0);
+    		identity.push_row(row);
+    	}
+    	return isomorphic(identity, 0, graph_strs);
+    }
+
+    GraphMatrix multiply(GraphMatrix & multiplicant) {
+    	GraphMatrix result(m_Size);
+    	for (int i = 0; i < m_Size; i++) {
+    		std::vector<int> row;
+    		result.push_row(row);
+    	}
+    	for (int i = 0; i < m_Size; i++)
+    		for (int j = 0; j < m_Size; j++) {
+    			int sum = 0;
+    			for (int k = 0; k < m_Size; k++)
+    				sum += m_Values[i][k] * multiplicant.v()[k][j];
+    			result.v()[i].push_back(sum);
+    		}
+    	return result;
+    }
+
+    bool is_trivial() {
+    	std::set<int> set;
+    	for (int i = 0; i < m_Size; i++)
+    		for (int j = 0; j < m_Size; j++)
+    			if (m_Values[i][j] && set.find(m_Values[i][j]) == set.end())
+    				set.insert(m_Values[i][j]);
+    	return set.size() < 2;
+    }
 };
 
-void print(int word, int wordlen) {
+GraphMatrix & print(int word, int wordlen) {
     int buffer = word;
     for (int i = 0; i < wordlen; i++) {
         if (buffer & 1)
@@ -113,50 +177,21 @@ void maximumstrategy(int wordlength, int maximalStrategy, int prefixSize) {
     }
 }
 
-//void printMatrix(std::vector<std::vector<int>> & matrix, int cnt) {
-//    for (int i = 0; i < cnt; i++) {
-//        for (int j = 0; j < cnt; j++)
-//            std::cout << matrix[i][j] << ' ';
-//        std::cout << std::endl;
-//    }
-//    std::cout << std::endl;
-//}
-//
-//std::string convertToString(std::vector<std::vector<int>> matrix, int cntNodes) {
-//	std::string s;
-//	std::vector<std::string> subwords;
-//	std::string buffer;
-//	for (int node = 0; node < cntNodes; node++) {
-//		for (int i = 0; i < cntNodes; i++)
-//			buffer.push_back(matrix[i][node] + 48);
-//		for (int j = 0; j < cntNodes; j++)
-//			buffer.push_back(matrix[node][j] + 48);
-//		subwords.push_back(buffer);
-//		buffer.clear();
-//	}
-//	std::sort(subwords.begin(), subwords.end());
-//	for (std::string & subword : subwords)
-//		s.append(subword);
-//	return s;
-//}
-
 std::set<std::string> nonisographs;
-bool isomorphic(GraphMatrix & matrix) {
-	std::string s = matrix.to_string();
-	if (nonisographs.find(s) == nonisographs.end()) {
-		nonisographs.insert(s);
-		return true;
-	}
-	return false;
-}
 
 void generateGraphs(int i, int j, GraphMatrix & matrix, int numSymbols) {
 
     if (i == matrix.size()) {
-        if (matrix.isSCGraph() && !isomorphic(matrix)) {
-        	matrix.print();
-        	std::cout << std::endl;
-        }
+//        if (
+//        		//!matrix.is_trivial() &&
+//				matrix.isSCGraph()
+//				//&&
+//				//!matrix.isomorphic_to(nonisographs)
+//				) {
+        	nonisographs.insert(matrix.to_string());
+
+        	//matrix.print();
+//        }
         return;
     }
     int nexti;
@@ -189,32 +224,30 @@ void generateGraphs(int cntNodes, int numSymbols) {
 
 int main(int argc, char** argv) {
 
-    int cntNodes = 3;
-//    GraphMatrix matrix1(cntNodes);
-//    std::vector<int> row10 = { 1, 1, 1 };
-//    std::vector<int> row11 = { 1, 1, 1 };
-//    std::vector<int> row12 = { 1, 1, 1 };
-//    matrix1.push_row(row10);
-//    matrix1.push_row(row11);
-//    matrix1.push_row(row12);
-//    matrix1.print();
-//    std::cout << std::boolalpha << matrix1.isSCGraph() << std::endl;
-//    GraphMatrix matrix2(cntNodes);
-//    std::vector<int> row20 = { 0, 1, 2 };
-//    std::vector<int> row21 = { 1, 0, 2 };
-//    std::vector<int> row22 = { 2, 2, 2 };
-//    matrix2.push_row(row20);
-//    matrix2.push_row(row21);
-//    matrix2.push_row(row22);
-//    matrix2.print();
-//
-//    std::cout << matrix1.to_string() << std::endl;
-//    std::cout << matrix2.to_string() << std::endl;
-    //std::cout << std::boolalpha << isSCGraph(matrix, cntNodes) << std::endl;
+      int cntNodes = 3;
+    GraphMatrix matrix1(cntNodes);
+    std::vector<int> row10 = { 2, 2, 2 };
+    std::vector<int> row11 = { 2, 0, 1 };
+    std::vector<int> row12 = { 2, 1, 0 };
+    matrix1.push_row(row10);
+    matrix1.push_row(row11);
+    matrix1.push_row(row12);
+    matrix1.print();
+    std::cout << std::endl;
+    GraphMatrix matrix2(cntNodes);
+    std::vector<int> row20 = { 0, 1, 2 };
+    std::vector<int> row21 = { 1, 0, 2 };
+    std::vector<int> row22 = { 2, 2, 2 };
+    matrix2.push_row(row20);
+    matrix2.push_row(row21);
+    matrix2.push_row(row22);
+    matrix2.print();
+    std::cout << std::endl;
+    nonisographs.insert(matrix1.to_string());
+    std::cout << std::boolalpha << matrix2.isomorphic_to(nonisographs) << std::endl;
 
 
-
-	generateGraphs(cntNodes, 2);
-    std::cout << nonisographs.size() << std::endl;
+	//generateGraphs(cntNodes, 2);
+    //std::cout << nonisographs.size() << std::endl;
     return 0;
 }
