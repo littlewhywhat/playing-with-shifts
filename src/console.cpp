a191f8f96f6e4a28522cee3dd496b998765d7829
littlewhywhat <littlewhywhat@gmail.com>
2017-03-19 10:45:00 +0100

    formated Console; added check for Game not null in Console::play

--- src/console.cpp
+++ src/console.cpp
@@ -4,15 +4,18 @@
 #include "language.h"
 #include "strategy.h"
 
-void Console::set_setup(const std::string & setup) {
-    m_Setup = setup;
-}
 void Console::load(const uint32_t & game_mode) {
     m_Game = GameFactory::get() -> create_instance(game_mode, m_WordLen);
     if (!m_Game)
         throw "No such game mode.";
     for (auto & word : m_WordSet)
         game().add_word(word);
+}
+
+bool Console::play(const Strategy &strategy) const {
+    if (!m_Game)
+        throw "There is no game loaded.";
+    return game().play(strategy);
 }
 
 void Console::print_lang() const {
--- src/console.h
+++ src/console.h
@@ -12,26 +12,27 @@ class Console {
     std::ostream & m_Out;
     Game * m_Game;
     uint32_t m_WordLen;
+    std::string m_Setup;
     Game & game() const { return *m_Game; }
   protected:
-    std::string m_Setup;
     std::set<std::string> m_WordSet;
-    void set_wordset(const std::set<std::string> & wordset) {
-        m_WordSet = wordset;
-    }
   public:
-    Console(std::ostream & out) : m_Out(out), m_Game(NULL), m_WordLen() {}
+    Console(std::ostream & out) : m_Out(out), m_Game(NULL), m_WordLen(0) {}
     virtual ~Console() { reset(); }
+
+    void set_setup(const std::string & setup) { m_Setup = setup; };
+    void set_wordlen(uint32_t val) { m_WordLen = val; }
+
     std::ostream & out() { return m_Out; }
-    void set_setup(const std::string & setup);
-    virtual void start() = 0;
     const std::string & setup() const { return m_Setup; }
     const uint32_t & wordlen() const { return m_WordLen; }
+
     void print_lang() const;
+
     void reset() { delete m_Game; }
-    void set_wordlen(uint32_t val) { m_WordLen = val; }
+    virtual void start() = 0;
     void load(const uint32_t & game_mode);
-    bool play(const Strategy & strategy) const { return game().play(strategy); } 
+    bool play(const Strategy & strategy) const;
 };
 
 #endif
