3216087d93cc481773a485fa52d9b81b64ea67b4
Roman Vaivod <vaivorom@fit.cvut.cz>
2017-03-21 17:51:35 +0100

    simplified if condition in method TrGame::play_rem

--- src/trgame.cpp
+++ src/trgame.cpp
@@ -1,15 +1,10 @@
 #include "trlanguage.h"
 #include "game.h"
-#include "tnode.h"
 #include "trgame.h"
 
 bool TrGame::play_rem(const TNode & node, uint64_t rem_strat, uint32_t wordlen) const {
-    if (node.is_leaf()) {
-        if (wordlen == 0)
-            return true;
-        else
-            return false;
-    }
+    if (node.is_leaf())
+        return wordlen == 0;
     if (rem_strat & 1)
         return node.has_zero() && node.has_one() &&
                play_rem(node.zero(), rem_strat >> 1, wordlen - 1) && 
