a46a7f41ffd0051ebdd9d6681df93e564a1a1487
Roman Vaivod <vaivorom@fit.cvut.cz>
2017-07-05 16:35:07 +0200

    separated logic from header, put get_langservice method as private

--- src/reallanguageservice.cpp
+++ src/reallanguageservice.cpp
@@ -3,3 +3,31 @@
 //
 
 #include "reallanguageservice.h"
+
+#include "graphlanguageservice.h"
+#include "filelanguageservice.h"
+#include "no3inrow.h"
+
+RealLanguageService::RealLanguageService() {
+    m_LangServices["-g"] = new GraphLanguageService();
+    m_LangServices["-l"] = new FileLanguageService();
+    m_LangServices["-b"] = new No3inRow();
+}
+
+RealLanguageService::~RealLanguageService() {
+    for (auto & service_entry : m_LangServices)
+        delete service_entry.second;
+}
+
+const LanguageService & RealLanguageService::get_langservice(const std::string &hostid) const {
+    if (m_LangServices.find(hostid) == m_LangServices.end())
+        throw "no such lang service";
+    auto & service_entry = *m_LangServices.find(hostid);
+    return *service_entry.second;
+}
+
+void RealLanguageService::load(const std::string & hostid, const std::string & lang_id,
+          Language & lang, const uint32_t & wordlen) const {
+    const LanguageService & service = get_langservice(hostid);
+    service.load(lang_id, lang, wordlen);
+}
--- src/reallanguageservice.h
+++ src/reallanguageservice.h
@@ -5,41 +5,20 @@
 #ifndef SHIFTS_REALLANGUAGESERVICE_H
 #define SHIFTS_REALLANGUAGESERVICE_H
 
-
 #include <string>
 #include <map>
 #include "language.h"
-#include "graphlanguageservice.h"
-#include "filelanguageservice.h"
-#include "no3inrow.h"
+#include "languageservice.h"
 
 class RealLanguageService {
   private:
     std::map<std::string, LanguageService *> m_LangServices;
+    const LanguageService &get_langservice(const std::string &hostid) const;
   public:
-    RealLanguageService() {
-        m_LangServices["-g"] = new GraphLanguageService();
-        m_LangServices["-l"] = new FileLanguageService();
-        m_LangServices["-b"] = new No3inRow();
-    }
-    ~RealLanguageService() {
-        for (auto & service_entry : m_LangServices)
-            delete service_entry.second;
-    }
-
-    const LanguageService &get_langservice(const std::string &hostid) const {
-        if (m_LangServices.find(hostid) == m_LangServices.end())
-            throw "no such lang service";
-        auto & service_entry = *m_LangServices.find(hostid);
-        return *service_entry.second;
-    }
-
+    RealLanguageService();
+    ~RealLanguageService();
     void load(const std::string & hostid, const std::string & lang_id,
-              Language & lang, const uint32_t & wordlen) const {
-        const LanguageService & service = get_langservice(hostid);
-        service.load(lang_id, lang, wordlen);
-    }
-
+              Language & lang, const uint32_t & wordlen) const;
 };
 
 
